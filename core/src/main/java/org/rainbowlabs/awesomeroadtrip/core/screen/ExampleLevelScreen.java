644faa15f27929f84bbea5546ff237f83d510ae8
Nico Leuchter <n.leuchter@nedeco.de>
2019-04-10 10:08:59 +0200

    Pause Feature WIP

--- core/src/main/java/org/rainbowlabs/awesomeroadtrip/core/screen/ExampleLevelScreen.java
+++ core/src/main/java/org/rainbowlabs/awesomeroadtrip/core/screen/ExampleLevelScreen.java
@@ -3,11 +3,13 @@ package org.rainbowlabs.awesomeroadtrip.core.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -23,8 +25,11 @@ public class ExampleLevelScreen implements Screen {
     private float rotationSpeed;
     private SpriteBatch batch;
     private Sprite mapSprite;
+    private ShapeRenderer shapeRenderer;
     static final int WORLD_WIDTH = 100;
     static final int WORLD_HEIGHT = 100;
+
+    private boolean GAME_PAUSED = false;
 
 
     public ExampleLevelScreen(AwesomeRoadTrip awesomeRoadTrip) {
@@ -37,6 +42,10 @@ public class ExampleLevelScreen implements Screen {
         stage = new Stage(new ScreenViewport());
         background = new Texture("Lvl2.jpg");
         Gdx.input.setInputProcessor(stage);
+        shapeRenderer = new ShapeRenderer();
+
+
+
 
 
         // Constructs a new OrthographicCamera, using the given viewport width and height
@@ -53,19 +62,50 @@ public class ExampleLevelScreen implements Screen {
     @Override
     public void render(float delta) {
 
-        handleInput();
-        cam.update();
-        batch.setProjectionMatrix(cam.combined);
+        if (!GAME_PAUSED) {
+            handleInput();
+            cam.update();
+            batch.setProjectionMatrix(cam.combined);
 
-        // clear the screen ready for next set of images to be drawn
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+            // clear the screen ready for next set of images to be drawn
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        batch.begin();
-        mapSprite.draw(batch);
-        batch.end();
+
+            batch.begin();
+            mapSprite.draw(batch);
+            batch.end();
+        }else{
+            handleInput();
+            Gdx.gl.glClearColor(1, 1, 1, 1);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+            Gdx.gl.glEnable(GL20.GL_BLEND);
+            Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+            Gdx.gl.glDisable(GL20.GL_BLEND);
+
+            batch.setProjectionMatrix(cam.combined);
+
+            batch.begin();
+            //sprite.draw(batch);
+            batch.end();
+
+            shapeRenderer.setColor(Color.BLACK);
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+            shapeRenderer.circle(50, 50, 32);
+            shapeRenderer.end();
+        }
     }
 
-    private void handleInput() {
+    private void handleInput(){
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
+            GAME_PAUSED = !GAME_PAUSED;
+        }
+        if (!GAME_PAUSED){
+            handleCamInput();
+        }
+    }
+
+    private void handleCamInput() {
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
             cam.zoom += 0.02;
         }
@@ -111,6 +151,7 @@ public class ExampleLevelScreen implements Screen {
     public void show() {
 
     }
+
 
     @Override
     public void hide() {
